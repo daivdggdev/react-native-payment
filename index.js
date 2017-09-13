cfb6ead33669613c792b686aaf37604f4830f6c5
daivdggdev <daivdggdev@163.com>
2017-09-13 16:58:01 +0800

    *): 接口封装, 发布1.0.2版本

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
 var RNPayment = require('react-native').NativeModules.RNPayment;
-module.exports = RNPayment;
+module.exports = {
+  registerApp: function(appId) {
+    return RNPayment ? RNPayment.registerApp(appId) : false;
+  },
+
+  alipay: function(orderInfo) {
+    return RNPayment ? RNPayment.alipay(orderInfo) : false;
+  },
+
+  wxpay: function(orderInfo) {
+    return RNPayment ? RNPayment.wxpay(orderInfo) : false;
+  }
+
+};
